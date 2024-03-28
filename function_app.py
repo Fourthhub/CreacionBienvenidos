935542f1e275e1479e745bb12998a5090e5c9220
diego <diegoechaure@gmail.com>
2024-03-28 21:13:45 +0100

    few

--- function_app.py
+++ function_app.py
@@ -10,20 +10,16 @@ import azure.functions as func
 
 app = func.FunctionApp()
 def enviarMail():
-
     message = Mail(
         from_email='from_email@example.com',
         to_emails='to@example.com',
         subject='Sending with Twilio SendGrid is Fun',
         html_content='<strong>and easy to do anywhere, even with Python</strong>')
-    try:
-        sg = SendGridAPIClient("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")
-        response = sg.send(message)
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
-    except Exception as e:
-        print(str(e))
+    sg = SendGridAPIClient("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")
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
 
 @app.schedule(schedule="0 0 10 * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
@@ -32,7 +28,4 @@ def crecionBienvenido(myTimer: func.TimerRequest) -> None:
  
     enviarMail()
     
-
-    
-
     logging.info('Python timer trigger function executed.')
