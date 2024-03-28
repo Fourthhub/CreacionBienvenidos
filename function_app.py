5cf0016d00a243b5f74c9f2748348a592fc3a5d4
diego <diegoechaure@gmail.com>
2024-03-28 22:01:05 +0100

    fewfew

--- function_app.py
+++ function_app.py
@@ -11,16 +11,20 @@ import azure.functions as func
 app = func.FunctionApp()
 def enviarMail():
     message = Mail(
-    from_email='from_email@example.com',
-    to_emails='to@example.com',
+    from_email='reservas@apartamentoscantabria.net',
+    to_emails='diegoechaure@gmail.com',
     subject='Sending with Twilio SendGrid is Fun',
     html_content='<strong>and easy to do anywhere, even with Python</strong>')
-    
-    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-    response = sg.send(message)
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+         logging.error(f"Error en la función: {str(e)}")
+
+
     
 
 @app.schedule(schedule="0 0 10 * * *", arg_name="myTimer", run_on_startup=True,
