d33ebea992625601f6ab4554e527692cb2a06b6f
diego <diegoechaure@gmail.com>
2024-03-28 21:46:31 +0100

    sedd

--- function_app.py
+++ function_app.py
@@ -2,19 +2,27 @@
 import logging
 import os
 import base64
-import requests
 from io import BytesIO
 from weasyprint import HTML
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail, Attachment, FileContent, FileName, FileType, Disposition
 import azure.functions as func
 
-def send_simple_message():
-	return requests.post(
-		"https://api.mailgun.net/v3/sandbox362f74d18132446fb3275a04b3426ccb.mailgun.org/messages",
-		auth=("api", "5e590bb029cdc79ceed83d3475b10659-f68a26c9-d0f10be3"),
-		data={"from": "Mailgun Sandbox <postmaster@sandbox362f74d18132446fb3275a04b3426ccb.mailgun.org>",
-			"to": "diego <diegoechaure@gmail.com>",
-			"subject": "Hello diego",
-			"text": "Congratulations diegoewewfewefw, you just sent an email fekaaaa with Mailgun! You are truly awesome!"})
+app = func.FunctionApp()
+def enviarMail():
+    message = Mail(
+    from_email='from_email@example.com',
+    to_emails='to@example.com',
+    subject='Sending with Twilio SendGrid is Fun',
+    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e.message)
 
 @app.schedule(schedule="0 0 10 * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
